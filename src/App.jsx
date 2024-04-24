f24163a8798eef54ac9c893dd30887095dcf9320
sifan <sifan007sifu@gmail.com>
2024-04-24 11:53:11 +0530

    suspens changed simple version

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import React, { Suspense, useEffect, useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React, { Suspense } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Loader from './components/loader/Loader';
 import ErrorPage from './pages/ErrorPage';
-// import Home from './pages/Home';
-// import About from './pages/About';
-// import Contact from './pages/Contact';
-// import Projects from './pages/Projects';
-// import Layout from './pages/Layout';
-// import ProjectDetails from './pages/ProjectDetails';
 
+// Lazy-load components
 const Layout = React.lazy(() => import('./pages/Layout'));
 const Home = React.lazy(() => import('./pages/Home'));
 const About = React.lazy(() => import('./pages/About'));
@@ -16,22 +11,28 @@ const Contact = React.lazy(() => import('./pages/Contact'));
 const Projects = React.lazy(() => import('./pages/Projects'));
 const ProjectDetails = React.lazy(() => import('./pages/ProjectDetails'));
 
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path='/' element={<Suspense fallback={<Loader status={true} />}><Layout /> </Suspense>}>
-        <Route path='/' element={<Suspense fallback={<Loader status={true} />}><Home /> </Suspense>} />
-        <Route path='/about' element={<Suspense fallback={<Loader status={true} />}><About /> </Suspense>} />
-        <Route path='/contact' element={<Suspense fallback={<Loader status={true} />}><Contact /> </Suspense>}/>
-        <Route path='/projects' element={<Suspense fallback={<Loader status={true} />}><Projects /> </Suspense>}/>
-        <Route path='/project/details' element={<Suspense fallback={<Loader status={true} />}><ProjectDetails /> </Suspense>} />
-        <Route path='/*' element={<ErrorPage />} />
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<Loader status={true} />}>
+              <Layout />
+            </Suspense>
+          }
+        >
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/project/details" element={<ProjectDetails />} />
+          <Route path="/*" element={<ErrorPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
