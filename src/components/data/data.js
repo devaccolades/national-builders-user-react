57d7002f9a4adca5acf3256025f465e2dff0b7fa
sifan <sifan007sifu@gmail.com>
2024-07-22 22:32:39 +0530

    add redirection links

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -67,4 +67,376 @@ export const oldUrlAndNewurl = [
         old:'/media-and-news',
         new:'/news-and-events'
     },
+    {
+        "old": "/contest/",
+        "new": "/news-and-events"
+      },
+      {
+        "old": "/tag/onam/",
+        "new": "/news-and-events"
+      },
+      {
+        "old": "/testimonials/ratnakumar/",
+        "new": "/testimonial"
+      },
+      {
+        "old": "/key_handovers/1/",
+        "new": "/key-handover"
+      },
+      {
+        "old": "/key_handovers/3/",
+        "new": "/key-handover"
+      },
+      {
+        "old": "/key_handovers/7/",
+        "new": "/key-handover"
+      },
+      {
+        "old": "/key_handovers/4/",
+        "new": "/key-handover"
+      },
+      {
+        "old": "/blog-new/",
+        "new": "/blog"
+      },
+      {
+        "old": "/key_handovers/5/",
+        "new": "/key-handover"
+      },
+      {
+        "old": "/location/ernakulam/",
+        "new": "/projects"
+      },
+      {
+        "old": "/testimonials/boban-varghese/",
+        "new": "/testimonial"
+      },
+      {
+        "old": "/custom-home-building/",
+        "new": "/blog/personalized-living-spaces"
+      },
+      {
+        "old": "/mansion-of-supremacy/",
+        "new": "/blog/mansion-of-supremacy"
+      },
+      {
+        "old": "/tag/3bhk-flats/",
+        "new": "/projects/national-majestic"
+      },
+      {
+        "old": "/amenities/cc-tv/",
+        "new": "/projects"
+      },
+      {
+        "old": "/tag/top-builders/",
+        "new": "/"
+      },
+      {
+        "old": "/director/sijo-sunny/",
+        "new": "/about"
+      },
+      {
+        "old": "/tag/onam-celebration/",
+        "new": "/news-and-events"
+      },
+      {
+        "old": "/tag/onam-blog/",
+        "new": "/news-and-events"
+      },
+      {
+        "old": "/tag/onam-festival/",
+        "new": "/news-and-events"
+      },
+      {
+        "old": "/testimonials/g-venugopal/",
+        "new": "/testimonial"
+      },
+      {
+        "old": "/amenities/ac-hall/",
+        "new": "/projects"
+      },
+      {
+        "old": "/author/nationaladmin/page/3/",
+        "new": "/projects"
+      },
+      {
+        "old": "/tag/apartments-in-kochi/",
+        "new": "/projects/luxury-flats-for-sale-in-kochi/"
+      },
+      {
+        "old": "/category/chengannur/page/2/",
+        "new": "/blog/who-is-the-best-builders-in-chengannur"
+      },
+      {
+        "old": "/author/nationaladmin/page/4/",
+        "new": "/projects"
+      },
+      {
+        "old": "/when-luxury-meets-comfort/",
+        "new": "/blog/when-luxury-meets-comfort"
+      },
+      {
+        "old": "/tag/apartments-national-builders/",
+        "new": "/"
+      },
+      {
+        "old": "/tag/apartments-in-mumbai/",
+        "new": "/projects/national-harmony-apartments-in-mumbai"
+      },
+      {
+        "old": "/tag/apartments-for-sale/",
+        "new": "/projects"
+      },
+      {
+        "old": "/director/anne-sijo-sunny/",
+        "new": "/about"
+      },
+      {
+        "old": "/rentals/national-harmony-4/",
+        "new": "/projects/national-harmony-apartments-in-mumbai"
+      },
+      {
+        "old": "/tag/flats-in-cochin/",
+        "new": "/projects"
+      },
+      {
+        "old": "/amenities/24-hour-security/",
+        "new": "/blog/what-are-the-popular-options-for-apartments-in-koc"
+      },
+      {
+        "old": "/testimonials/ruby-von-rails/",
+        "new": "/testimonial"
+      },
+      {
+        "old": "/rentals/national-pearlstar-2/",
+        "new": "/projects/national-pearl-star"
+      },
+      {
+        "old": "/tag/apartments-in-cochin/",
+        "new": "/projects"
+      },
+      {
+        "old": "/tag/2-bhk-flats/",
+        "new": "/projects"
+      },
+      {
+        "old": "/advantages-buying-home-during-launch/",
+        "new": "/blog/advantages-buying-home-during-launch"
+      },
+      {
+        "old": "/benefits-of-living-in-kochi/",
+        "new": "/blog/benefits-of-living-in-kochi"
+      },
+      {
+        "old": "/onam-the-hues-of-heredity/",
+        "new": "/news-and-events"
+      },
+      {
+        "old": "/testimonials/ruby-von-rails-2/",
+        "new": "/testimonial"
+      },
+      {
+        "old": "/tag/flats-in-fort-kochi/",
+        "new": "/projects/luxury-flats-for-sale-in-kochi/"
+      },
+      {
+        "old": "/tag/top-builders-in-kochi/",
+        "new": "/"
+      },
+      {
+        "old": "/rentals/national-residency-o-14/",
+        "new": "/rentals"
+      },
+      {
+        "old": "/tag/ready-to-occupy-flats/",
+        "new": "/blog/ready-to-move-flats-in-kochi"
+      },
+      {
+        "old": "/testimonials/jimple-paul-sheela-thomas/",
+        "new": "/testimonial"
+      },
+      {
+        "old": "/tag/apartment-builders-in-kochi/",
+        "new": "/"
+      },
+      {
+        "old": "/tag/ready-to-move-apartments/",
+        "new": "/blog/ready-to-move-flats-kochi-perfect-first-time-homeb"
+      },
+      {
+        "old": "/category/top-builders-in-chengannur/",
+        "new": "/blog/who-is-the-best-builders-in-chengannur"
+      },
+      {
+        "old": "/location/apartments-for-sale-thiruvalla/",
+        "new": "/projects"
+      },
+      {
+        "old": "/tag/leading-builders-in-kochi/",
+        "new": "/"
+      },
+      {
+        "old": "/tag/premium-builders-in-kochi/",
+        "new": "/"
+      },
+      {
+        "old": "/tag/best-flats-in-chengannur/",
+        "new": "/blog/who-is-the-best-builders-in-chengannur"
+      },
+      {
+        "old": "/tag/top-builders-in-chengannur/",
+        "new": "/projects"
+      },
+      {
+        "old": "/tag/luxury-apartments-in-mumbai/",
+        "new": "/projects/national-harmony-apartments-in-mumbai"
+      },
+      {
+        "old": "/key_handovers/2/",
+        "new": "/key-handover"
+      },
+      {
+        "old": "/a-destination-where-luxury-meets-satisfaction/",
+        "new": "/blog/a-destination-where-luxury-meets-satisfaction"
+      },
+      {
+        "old": "/tag/flats-for-sale-in-kochi/",
+        "new": "/projects"
+      },
+      {
+        "old": "/ready-to-move-flats-in-kochi/",
+        "new": "/projects/ready-to-move-flats-in-kochi"
+      },
+      {
+        "old": "/investing-wisely-considerations-buying-flats-kochi/",
+        "new": "/blog/investing-wisely-considerations-buying-flats-kochi"
+      },
+      {
+        "old": "/media/marunadum-malanadum-m-c-sunny/",
+        "new": "/news-and-events/marunadum-malanadum-m-c-sunny"
+      },
+      {
+        "old": "/location/flats-for-sale-navi-mumbai/",
+        "new": "/projects"
+      },
+      {
+        "old": "/key_handovers/mr-arun-koli-national-harmony/",
+        "new": "/key-handover"
+      },
+      {
+        "old": "/key_handovers/miss-nikita-kamath-national-harmony/",
+        "new": "/key-handover"
+      },
+      {
+        "old": "/testimonials/sabari-prakash-p-v-ranjitha/",
+        "new": "/testimonial"
+      },
+      {
+        "old": "/mistakes-to-avoid-while-buying-an-apartment/",
+        "new": "/blog/ready-to-move-flats-in-kochi"
+      },
+      {
+        "old": "/who-is-the-best-builders-in-chengannur/",
+        "new": "/blog/who-is-the-best-builders-in-chengannur"
+      },
+      {
+        "old": "/key_handovers/mrs-sharada-m-nair-national-harmony/",
+        "new": "/key-handover"
+      },
+      {
+        "old": "/sky-villa-apartments-to-make-your-life-elite/",
+        "new": "/blog/sky-villa-apartments-to-make-your-life-elite"
+      },
+      {
+        "old": "/best-residential-areas-to-buy-apartments-in-kochi/",
+        "new": "/blog/emerging-real-estate-hotspots-in-kochi-where-to-b"
+      },
+      {
+        "old": "/location/flats-for-sale-in-kochi/page/2/",
+        "new": "/projects"
+      },
+      {
+        "old": "/things-to-know-about-luxury-apartments-in-mumbai/",
+        "new": "/blog/the-high-end-lifestyle-in-mumbai-the-blog-about-ou"
+      },
+      {
+        "old": "/location/flats-for-sale-in-kochi/page/3/",
+        "new": "/projects"
+      },
+      {
+        "old": "/location/flats-for-sale-navi-mumbai/page/2/",
+        "new": "/projects"
+      },
+      {
+        "old": "/small-space-big-style-transforming-your-2-bhk-flat/",
+        "new": "/blog/small-space-big-style-transforming-your-2-bhk-flat"
+      },
+      {
+        "old": "/real-estate-investment-in-residential-apartments-with-promising-returns/",
+        "new": "/blog/real-estate-investment-in-residential-apartments-w"
+      },
+      {
+        "old": "/benefits-of-buying-a-ready-to-move-in-apartment/",
+        "new": "/blog/real-estate-investment-in-residential-apartments-w"
+      },
+      {
+        "old": "/investing-in-kochi-real-estate-key-strategies-for-maximizing-returns/",
+        "new": "/blog/real-estate-investment-in-residential-apartments-w"
+      },
+      {
+        "old": "/which-is-the-best-place-to-buy-apartments-in-mumbai/",
+        "new": "/projects/national-harmony-apartments-in-mumbai"
+      },
+      {
+        "old": "/looking-for-a-flat-in-kochi-at-an-affordable-price/",
+        "new": "/blog/affordable-ready-to-move-in-flats-in-kochi-where-t"
+      },
+      {
+        "old": "/which-are-the-best-residential-areas-and-flats-in-kochi/",
+        "new": "/blog/emerging-real-estate-hotspots-in-kochi-where-to-b"
+      },
+      {
+        "old": "/why-do-people-choose-kochi-as-a-future-home-investment/",
+        "new": "/blog/why-do-people-choose-kochi-as-a-future-"
+      },
+      {
+        "old": "/a-comprehensive-guide-to-apartments-in-kochi-the-ultimate-living-experience/",
+        "new": "/blog/a-comprehensive-guide-to-apartments-in-kochi-"
+      },
+      {
+        "old": "/renting-vs-buying-whats-the-right-choice-for-you-in-kochi/",
+        "new": "/blog/renting-vs-buying-whats-the-right-choice-for-you-"
+      },
+      {
+        "old": "/exploring-the-amenities-what-to-expect-in-luxury-apartments-in-kochi/",
+        "new": "/blog/exploring-the-amenities-what-to-expect-in-luxury-a"
+      },
+      {
+        "old": "/why-an-apartment-in-kochi-is-a-promising-real-estate-investment/",
+        "new": "/blog/why-do-people-choose-kochi-as-a-future-"
+      },
+      {
+        "old": "/how-to-calculate-the-total-cost-of-buying-a-flat-in-mumbai/",
+        "new": "/projects/national-harmony-apartments-in-mumbai"
+      },
+      {
+        "old": "/emerging-real-estate-hotspots-in-kochi-where-to-buy-flats-for-future-growth/",
+        "new": "/blog/emerging-real-estate-hotspots-in-kochi-where-to-b"
+      },
+      {
+        "old": "/projects/locations/flats-for-sale-in-kochi",
+        "new": "/blog/ready-to-move-flats-in-kochi"
+      },
+      {
+        "old": "/why-kochi-is-the-ideal-location-for-your-dream-home-top-projects-in-kochi/",
+        "new": "/blog/why-kochi-is-the-ideal-location-for-your-dream-hom"
+      },
+      {
+        "old": "/is-now-the-time-to-invest-in-a-flat-in-kochis-booming-real-estate-market/",
+        "new": "/blog/is-now-the-time-to-invest-in-a-flat-in-kochis-b"
+      },
+      {
+        "old": "/what-are-the-popular-options-for-apartments-in-kochi-and-what-are-the-amenities-and-features-national-signature-offer/",
+        "new": "/blog/what-are-the-popular-options-for-apartments-in-koc"
+      }
 ]
